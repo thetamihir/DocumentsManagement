ad3059ab70bde8a1ca41dbcbb4a56612f6224256
thetamihir <mihir.shingala.thetatechnolabs@gmail.com>
2023-05-09 10:27:26 +0530

    Initial Commit

---
+++ DocumentManagementSystem/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DocumentManagementSystem
+//
+//  Created by Mihir Shingala on 09/05/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
